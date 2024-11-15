f1cf8b18894fb264f54605eaa6c700e313411378
PurpliX.. <dasha.ajufoh@binghamuni.edu.ng>
2024-11-15 14:23:18 +0100

    organized

---
+++ main.py
@@ -0,0 +1,22 @@
+#print("Hello World")
+#first_name='Diane'
+#last_name='Trump'
+age='16'
+height=5
+
+
+
+first_name=input('Enter first name:')
+last_name=input('Enter last name:')
+
+age=int(input('Enter your age:'))
+height=float(input('Enter your height'))
+
+print(age*5)
+
+
+
+
+
+
+#print(first_name,last_name
---
+++ main2.py
@@ -0,0 +1,22 @@
+age=int(input('Enter your age:'))
+
+height=float(input('Entr your height:'))
+
+
+if height > 168:
+    print('You are tall enough')
+elif age > 17:
+    print('You can join the ride my dude!')
+else:
+    print('You cannot join the ride!')
+
+
+
+
+
+
+
+
+
+
+    
