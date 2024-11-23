1d83b30c558156ec71cb9d9048d973b5bf01b775
PurpliX.. <dasha.ajufoh@binghamuni.edu.ng>
2024-11-23 18:57:51 +0100

    first commit

---
+++ COS_101.second class.py
@@ -0,0 +1,57 @@
+#def calculate_v():
+
+ #   u=float(input('Enter the value for initial velocity:'))
+  #  a=int(input('Enter the value for acceleration:'))
+  #  t=int(input('Enter the value for time:'))
+
+ #   return print(u+(a*t))
+
+#calculate_v()
+
+
+#WHILE LOOPS
+
+#while True:
+    #print('Hello World')
+
+#x=0
+#while x<10:
+ #   print(x)
+#    x=x+1
+
+#x=0
+#while x<501:
+   #  print(x)
+  #   x=x+2
+
+#x=501
+#while x>0:
+     #print(x)
+     #x=x-2
+
+#FOR RANGE.
+
+#for i in range(10):
+  #   print(i)
+
+#for i in range(5,20):
+ #    print(i)
+
+#for i in range(2,500,2):
+ #    print(i)
+
+#TEST.
+#x=999
+#while x>0:
+ #    print(x)
+#     x=x-2
+
+#CORRECTION
+for i in range(1000,0):
+     if(1 %2 ==1):
+          print(i)
+
+
+          
+
+
